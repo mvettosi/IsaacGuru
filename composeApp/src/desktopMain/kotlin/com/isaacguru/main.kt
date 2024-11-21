c28178f188154a1d516989d3e7662a2f75995ed7
Matteo Vettosi <matteo.vettosi@gmail.com>
2024-11-21 19:59:21 +0100

    Initial commit

---
+++ composeApp/src/desktopMain/kotlin/com/isaacguru/main.kt
@@ -0,0 +1,13 @@
+package com.isaacguru
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "IsaacGuru",
+    ) {
+        App()
+    }
+}
