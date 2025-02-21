77a3e0585c840226dcb0e8439b95555fb343304a
Matteo Vettosi <matteo.vettosi@gmail.com>
2025-02-21 01:21:00 +0000

    Bump to see if apis are active now

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -84,8 +84,8 @@ android {
     applicationId = "com.isaacguru"
     minSdk = libs.versions.android.minSdk.get().toInt()
     targetSdk = libs.versions.android.targetSdk.get().toInt()
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "0.0.2"
   }
   packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
   buildTypes { getByName("release") { isMinifyEnabled = false } }
