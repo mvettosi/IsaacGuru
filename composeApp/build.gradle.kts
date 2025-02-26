98fb643e4ac95c3a72f9c4c9355381b667b9e4fc
Matteo Vettosi <matteo.vettosi@gmail.com>
2025-02-26 22:40:11 +0100

    Updated Android app version to 0.0.4 and versionCode to 4.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -86,8 +86,8 @@ android {
     applicationId = "com.isaacguru"
     minSdk = libs.versions.android.minSdk.get().toInt()
     targetSdk = libs.versions.android.targetSdk.get().toInt()
-    versionCode = 3
-    versionName = "0.0.3"
+    versionCode = 4
+    versionName = "0.0.4"
   }
   packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
   buildTypes { getByName("release") { isMinifyEnabled = false } }
